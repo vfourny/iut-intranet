ace8d5d9108e546681ae8c934d2cea2c6b0d8677
Cuvelier Armand <armandcuvelier@gmail.com>
2026-05-06 16:05:22 +0200

    fix: pr issues

--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -1,13 +1,11 @@
 import type { inferRouterOutputs } from '@trpc/server'
 
 import { authRouter } from '@/modules/auth/auth.router'
-import { departmentRouter } from '@/modules/department/department.router'
 import { userRouter } from '@/modules/user/user.router'
 import { router } from '@/trpc'
 
 export const appRouter = router({
   auth: authRouter,
-  department: departmentRouter,
   user: userRouter,
 })
 
