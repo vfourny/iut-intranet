4705d2ccd335d294efc2a3ec848aa82e78f29f4c
Cuvelier Armand <armandcuvelier@gmail.com>
2026-05-11 14:10:17 +0200

    feat: installation calendar ScheduleX

---
+++ apps/web/src/components/event/calendar-object.vue
@@ -0,0 +1,13 @@
+<script setup="ts">
+import { createCalendar } from '@schedule-x/calendar'
+import { ScheduleXCalendar } from '@schedule-x/vue'
+import { Temporal } from 'temporal-polyfill'
+
+const calendar = createCalendar({
+  selectedDate: Temporal.PlainDate.from(DatetimeFormat),
+})
+</script>
+
+<template>
+  <ScheduleXCalendar :calendar-app="calendar" />
+</template>
